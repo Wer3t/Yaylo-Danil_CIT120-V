af5bb05e453938c9d23439859cd8ad1e19b7a1da
Wer3t <yaylo.danil@gmail.com>
2021-02-18 17:14:14 +0200

    lab15comments

---
+++ lab15/src/lib.c
@@ -0,0 +1,392 @@
+/**a
+ * @file lib.c
+ * @brief Файл з реалізацією функцій.
+ *
+ * @author Yaylo D.
+ * @date 15-feb-2021
+ * @version 1.0
+ */
+ 
+#include "lib.h"
+
+int count_lines(){
+	int lines_count=0;
+	FILE *file;
+	file = fopen("input.txt", "r");
+	while(! feof(file)){
+		if(fgetc(file)=='\n'){
+			lines_count++;
+		}
+	}
+	fclose(file);
+	return lines_count;
+}
+
+void read_file(struct bird types[], int n){
+	FILE *file;
+	file = fopen("input.txt", "r");
+	for(int i=0;i<n;i++){
+		fscanf(file, "%d",&types[i].ringed);
+		fscanf(file, "%s",&types[i].species);
+		fscanf(file, "%d",&types[i].age);
+		fscanf(file, "%d",&types[i].house.square);
+		fscanf(file, "%d",&types[i].house.height);
+		fscanf(file, "%d",&types[i].house.num_feeders);
+		fscanf(file, "%d",&types[i].house.nest);
+		fscanf(file, "%d",&types[i].sex);
+	}
+	fclose(file);
+}
+
+void write_file(struct bird types[], int n){
+	FILE *file;
+	file = fopen("output.txt", "w");
+	for(int i=0;i<n;i++){
+		if(types[i].ringed==2){
+			fprintf(file, "ringed, ");
+		}else if(types[i].ringed==1){
+			fprintf(file, "non ringed, ");
+		}
+		fprintf(file, "%s, ", types[i].species);
+		fprintf(file, "%d, ", types[i].age);
+		fprintf(file, "%d, ", types[i].house.square);
+		fprintf(file, "%d, ", types[i].house.height);
+		fprintf(file, "%d, ", types[i].house.num_feeders);
+		if(types[i].house.nest==2){
+			fprintf(file, "have nest, ");
+		}else if(types[i].house.nest==1){
+			fprintf(file, "haven't nest, ");
+		}
+		if(types[i].sex==2){
+			fprintf(file, "male");
+		}else if(types[i].sex==1){
+			fprintf(file, "female");
+		}
+		fprintf(file, "\n");
+	}
+	fclose(file);
+}
+	
+void write_out(struct bird types[], int n){
+	for(int i=0;i<n;i++){
+		if(types[i].ringed==2){
+			printf("ringed, ");
+		}else if(types[i].ringed==1){
+			printf("non ringed, ");
+		}
+		printf("%s, ", types[i].species);
+		printf("%d, ", types[i].age);
+		printf("%d, ", types[i].house.square);
+		printf("%d, ", types[i].house.height);
+		printf("%d, ", types[i].house.num_feeders);
+		if(types[i].house.nest==2){
+			printf("have nest, ");
+		}else if(types[i].house.nest==1){
+			printf("haven't nest, ");
+		}
+		if(types[i].sex==2){
+			printf("male");
+		}else if(types[i].sex==1){
+			printf("female");
+		}
+		printf("\n");
+	}
+}
+
+void sorting(struct bird types[], int criterion, int n){
+	switch(criterion){
+		case 1:
+			sort_yn(types, n, criterion);
+			break;
+		case 2:
+			sort_abc(types, n, criterion);
+			break;
+		case 3:
+			sort_increase(types, n, criterion);
+			break;
+		case 4:
+			sort_increase(types, n, criterion);
+			break;
+		case 5:
+			sort_increase(types, n, criterion);
+			break;
+		case 6:
+			sort_increase(types, n, criterion);
+			break;
+		case 7:
+			sort_yn(types, n, criterion);
+			break;
+		case 8:
+			sort_yn(types, n, criterion);
+			break;
+	}
+}
+
+void sort_abc(struct bird types[], int n, int criterion){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i;j++){
+			if(j+1==n){
+				break;
+			}else{
+				char temp1[20]={0};
+				char temp2[20]={0};
+				sprintf(temp1,types[j].species);
+				sprintf(temp2,types[j+1].species);
+				if(strcmp(temp1,temp2)>0){
+					sprintf(types[j].species,temp2);
+					sprintf(types[j+1].species,temp1);
+				}
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		printf("\n%s", types[i].species);
+	}
+}
+
+void sort_yn(struct bird types[], int n, int criterion){
+	struct bird sort[n];
+	int y =0;
+	int no =1;
+	if(criterion==1){
+		for(int i=0;i<n;i++){
+			if(types[i].ringed==2){
+				sprintf(sort[y].species,types[i].species);
+				y++;
+			}else{
+				sprintf(sort[n-no].species,types[i].species);
+				no++;
+			}
+		}
+	} else if(criterion==7){
+		for(int i=0;i<n;i++){
+			if(types[i].house.nest==2){
+				sprintf(sort[y].species,types[i].species);
+				y++;
+			}else{
+				sprintf(sort[n-no].species,types[i].species);
+				no++;
+			}
+		}
+	} else if(criterion==8){
+		for(int i=0;i<n;i++){
+			if(types[i].sex==2){
+				sprintf(sort[y].species,types[i].species);
+				y++;
+			}else{
+				sprintf(sort[n-no].species,types[i].species);
+				no++;
+			}
+		}
+	}
+	
+	for(int i=0;i<n;i++){
+		printf("\n%s", sort[i].species);
+	}
+}
+
+void sort_increase(struct bird types[], int n, int criterion){
+	if(criterion==3){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i;j++){
+				if(types[j].age>types[j+1].age){
+				int temp=types[j+1].age;
+				int temp1=types[j].age;
+				types[j].age=temp;
+				types[j+1].age=temp1;
+				char temp2[20]={0};
+				sprintf(temp2,types[j].species);
+				sprintf(types[j].species,types[j+1].species);
+				sprintf(types[j+1].species,temp2);
+				}
+			}
+		}
+	} else if(criterion==4){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i;j++){
+				if(types[j].house.square>types[j+1].house.square){
+				int temp=types[j+1].house.square;
+				int temp1=types[j].house.square;
+				types[j].house.square=temp;
+				types[j+1].house.square=temp1;
+				char temp2[20]={0};
+				sprintf(temp2,types[j].species);
+				sprintf(types[j].species,types[j+1].species);
+				sprintf(types[j+1].species,temp2);
+				}
+			}
+		}
+	} else if(criterion==5){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i;j++){
+				if(types[j].house.height>types[j+1].house.height){
+				int temp=types[j+1].house.height;
+				int temp1=types[j].house.height;
+				types[j].house.height=temp;
+				types[j+1].house.height=temp1;
+				char temp2[20]={0};
+				sprintf(temp2,types[j].species);
+				sprintf(types[j].species,types[j+1].species);
+				sprintf(types[j+1].species,temp2);
+				}
+			}
+		}
+	} else if(criterion==6){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i;j++){
+				if(types[j].house.num_feeders>types[j+1].house.num_feeders){
+				int temp=types[j+1].house.num_feeders;
+				int temp1=types[j].house.num_feeders;
+				types[j].house.num_feeders=temp;
+				types[j+1].house.num_feeders=temp1;
+				char temp2[20]={0};
+				sprintf(temp2,types[j].species);
+				sprintf(types[j].species,types[j+1].species);
+				sprintf(types[j+1].species,temp2);
+				}
+			}
+		}
+	}
+	
+	for(int i=0;i<n;i++){
+		printf("\n%s", types[i].species);
+	}
+}
+
+void percent_male_to_female(struct bird types[], int n){
+	double male=0, female=0;
+	for(int i=0;i<n;i++){
+		if(types[i].sex==1){
+			male++;
+		}else{
+			female++;
+		}
+	}
+	double percent_male= male/(male+female);
+	double percent_female= female/(male+female);
+	printf("\nПроцент мужских особей: %.2lf",percent_male);
+	printf("\nПроцент женских особей: %.2lf\n\n",percent_female);
+}
+
+int randm(){
+	int ch=rand()%230+23;
+	return ch;
+}
+int rand_2_1(){
+	int ch=rand()%2+1;
+	return ch;
+}
+void generation(){
+	int n=rand()%10;
+	struct bird types_r[n];
+	for(int i=0; i<n; i++){
+		types_r[i].ringed=rand_2_1();
+		sprintf(types_r[i].species,"bird number%d", i);
+		types_r[i].age=randm();
+		types_r[i].house.square=randm();
+		types_r[i].house.height=randm();
+		types_r[i].house.num_feeders=randm();
+		types_r[i].house.nest=rand_2_1();
+		types_r[i].sex=rand_2_1();
+	}
+	write_out(types_r, n);
+}
+
+void check(p){
+	if (p==NULL){
+		printf("\nЭлемент структуры отсутствует");
+	}
+}
+
+void read_file_bin(struct bird types[], int n){
+	FILE *file;
+	file = fopen("input.txt", "rb");
+	for(int i=0;i<n;i++){
+		fread(&types[i].ringed, 0,1,file);
+		int a = strchr(file, ' ');
+		fseek(file,2,1);
+		int b = strchr(file, ' ');
+		a=b-a;
+		fread(&types[i].species, sizeof(char),a,file);
+		fseek(file,2,1);
+		fread(&types[i].age, 0,1,file);
+		fseek(file,2,1);
+		fread(&types[i].house.square, 0,1,file);
+		fseek(file,2,1);
+		fread(&types[i].house.height, 0,1,file);
+		fseek(file,2,1);
+		fread(&types[i].house.num_feeders, 0,1,file);
+		fseek(file,2,1);
+		fread(&types[i].house.nest, 0,1,file);
+		fseek(file,2,1);
+		fread(&types[i].sex, 0,1,file);
+		fseek(file,2,1);
+	}
+	fclose(file);
+}
+
+void write_file_bin(struct bird types[], int n){
+	FILE *file;
+	file = fopen("output.txt", "wb");
+	for(int i=0;i<n;i++){
+		if(types[i].ringed==2){
+			fwrite("ringed, ", sizeof(char), 8, file);
+		}else if(types[i].ringed==1){
+			fwrite("non ringed, ", sizeof(char), 12, file);
+		}
+		int a=strlen(types[i].species);
+		fwrite(types[i].species, sizeof(char), a, file);
+		fseek(file,2,1);
+		fwrite(types[i].age, 0, 1, file);
+		fseek(file,2,1);
+		fwrite(types[i].house.square, 0, 1, file);
+		fseek(file,2,1);
+		fwrite(types[i].house.height, 0, 1, file);
+		fseek(file,2,1);
+		fwrite(types[i].house.num_feeders, 0, 1, file);
+		fseek(file,2,1);
+		if(types[i].house.nest==2){
+			fwrite("have nest, ", sizeof(char), 11, file);
+			
+		}else if(types[i].house.nest==1){
+			fwrite("haven't nest, ", sizeof(char), 14, file);
+		}
+		if(types[i].sex==2){
+			fwrite("male", sizeof(char), 4, file);
+		}else if(types[i].sex==1){
+			fwrite("female", sizeof(char), 6, file);
+		}
+		fseek(file,2,1);
+	}
+	fclose(file);
+}
+
+void write_struct_number(struct bird types[], int num){
+	FILE *file;
+	file = fopen("input.txt", "rb");
+	char rez[20];
+	int a,b;
+	if(num==1){
+		fscanf(file, "%s",rez);
+		printf("\n%s", rez);
+		
+	}else if(num==2){
+		a = strchr(file, ' ');
+		fseek(file,2*a+2,1);
+		fscanf(file, "%s",rez);
+		printf("\n%s", rez);
+		
+	}else{
+		fseek(file,7,1);
+		for(int i=0;i<num-2;i++){
+			a = strchr(file, ' ');
+			fseek(file,2,1);
+			b = strchr(file, ' ');
+			a=b-a;
+			fseek(file,2*a,1);
+		}
+		fscanf(file, "%s",rez);
+		printf("\n%s", rez);
+		fclose(file);
+	}
+}
---
+++ lab15/src/main.c
@@ -0,0 +1,100 @@
+/**
+ * @mainpage
+ * # Загальне завдання
+ * 1. **Створити** структуру, що відображає "базовий клас" та виконати
+ * індивідуальне завдання 
+ * @author Yaylo D.
+ * @date 15-feb-2021
+ * @version 1.0
+ */
+
+/**
+ * @file main.c
+ * @brief Файл з демонстрацією роботи структури та методів
+ * оперування ним.
+ *
+ * @author Yaylo D.
+ * @date 15-feb-2021
+ * @version 1.0
+ */
+
+#include "lib.h"
+
+/**
+ * Головна функція.
+ *
+ * Послідовність дій:
+ * - вивід результату роботи функції @function read_file
+ * - вивід результату роботи функції @function write_out
+ * - вивід результату роботи функції @function write_out
+ * - вивід результату роботи функції @function sorting
+ * - вивід результату роботи функції @function percent_male_to_female
+ * - вивід результату роботи функції @function task
+ * - вивід результату роботи функції @function generation
+ * @return успішний код повернення з програми (0)
+ */
+int main() {
+	printf("\n");
+	srand(time(0));
+	int n = count_lines();
+	struct bird types[n];
+	
+	read_file(types, n);
+	write_file(types, n);
+	write_out(types, n);
+	
+	int criterion=0;
+	printf("\nВведите критерий, по которому будут отсортированы структуры : \nОкольцованность птицы[1]                Высота дома[5]\nВид птицы[2]                            Количество кормушек в доме[6]\nВозраст птицы[3]                        Наличие гнезда[7]\nПлощадь дома[4]                         Пол птицы[8]\nВведите число: ");
+	scanf("%d", &criterion);
+	if(criterion>0&&criterion<9){
+	sorting(types, criterion, n);
+	}else{
+		printf("\nВведено неверное число!\n");
+	}
+	printf("\n");
+	read_file(types, n);
+	
+	percent_male_to_female(types, n);
+	
+	generation();
+	printf("\n");
+	
+	struct bird *p;
+	for(int i=0; i<n; i++){
+		check(p=types[i].ringed);
+		check(p=types[i].species);
+		check(p=types[i].age);
+		check(p=types[i].house.square);
+		check(p=types[i].house.height);
+		check(p=types[i].house.num_feeders);
+		check(p=types[i].house.nest);
+		check(p=types[i].sex);
+	}
+	
+	read_file_bin(types, n);
+	for(int i=0;i<n;i++){
+		printf( "%d, ", types[i].ringed);
+		printf( "%s, ", types[i].species);
+		printf( "%d, ", types[i].age);
+		printf( "%d, ", types[i].house.square);
+		printf( "%d, ", types[i].house.height);
+		printf("%d, ", types[i].house.num_feeders);
+		printf( "%d, ", types[i].house.nest);
+		printf( "%d ", types[i].sex);
+		printf("\n");
+		}
+	write_file_bin(types, n);
+	
+	printf("\nВведите индекс структуры, в которой нужно будет найти поле(от 0 до %d): ", n-1);
+	int num_a;
+	scanf("%d", &num_a);
+	printf("\nВведите поле, которое нужное найти : \nОкольцованность птицы[1]                Высота дома[5]\nВид птицы[2]                            Количество кормушек в доме[6]\nВозраст птицы[3]                        Наличие гнезда[7]\nПлощадь дома[4]                         Пол птицы[8]\nВведите число: ");
+	int num_b;
+	scanf("%d", &num_b);
+	int num=num_a*8+num_b;
+	write_struct_number(types, num);
+	
+	return 0;
+}
+
+
